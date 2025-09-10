fc26cebbaca682139e25921cd522c92bb7d5705b
twoChar <tushar20254@iiitd.ac.in>
2025-09-10 21:54:11 +0530

    deployed

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/mini-web-app/', // 👈 match exactly what your repo is named
   plugins: [react()],
-})
+  base: '/Weather-and-News-Aggregator-App-/',
+});
+
